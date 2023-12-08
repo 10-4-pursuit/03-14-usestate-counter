4fe33fc96558a1798678660d51d6bfd3b1403ee7
dttrue <danieltorres@pursuit.org>
2023-12-07 21:46:38 -0500

    Daniels add StatefulCounter component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StatefulCounter from './StatefulCounter';
+
+function App() {
+  return (
+    <div className="App">
+       <StatefulCounter />
+    </div>
+  );
+}
+
+export default App;
